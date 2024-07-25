6957c9f1f721f695a0362835ca9fbd58749f665d
Hugobsan <hugocorone10@gmail.com>
2024-07-25 17:49:10 -0300

    fix: corrigindo preview da imagem

--- contatize-app/resources/js/Pages/Contatos/ContatosCreate.vue
+++ contatize-app/resources/js/Pages/Contatos/ContatosCreate.vue
@@ -1,16 +1,16 @@
 <script setup>
-import { useForm } from '@inertiajs/vue3';
-import { ref, watch } from 'vue';
-import { useToast } from 'vue-toastification';
+import { useForm } from "@inertiajs/vue3";
+import { ref, watch } from "vue";
+import { useToast } from "vue-toastification";
 
 const dialog = ref(false);
 const toast = useToast();
 
 // Formulário para adicionar um novo contato
 const form = useForm({
-    nome: '',
-    telefone: '',
-    email: '',
+    nome: "",
+    telefone: "",
+    email: "",
     imagem: null,
 });
 
@@ -18,20 +18,20 @@ const form = useForm({
 const errors = ref({});
 
 const createContato = () => {
-    form.post(route('contatos.store'), {
+    form.post(route("contatos.store"), {
         onSuccess: () => {
             dialog.value = false;
             form.reset();
             // Resetando imagem
-            imagePreview.value = '';
-            toast.success('Contato adicionado com sucesso!');
+            imagePreview.value = "";
+            toast.success("Contato adicionado com sucesso!");
             errors.value = {}; // Limpar erros em caso de sucesso
         },
         onError: (errorResponse) => {
             // Formata os erros para o formato esperado pelo Vuetify
             errors.value = formatErrors(errorResponse);
-            toast.error('Erro ao adicionar contato!');
-        }
+            toast.error("Erro ao adicionar contato!");
+        },
     });
 };
 
@@ -39,7 +39,7 @@ watch(dialog, (value) => {
     if (!value) {
         form.reset();
         errors.value = {};
-        imagePreview.value = '';
+        imagePreview.value = "";
     }
 });
 
@@ -54,7 +54,7 @@ const previewImage = (event) => {
     }
 };
 
-const imagePreview = ref('');
+const imagePreview = ref("");
 
 const formatErrors = (errorResponse) => {
     const formattedErrors = {};
@@ -73,7 +73,7 @@ const formatErrors = (errorResponse) => {
                 <v-card-title>Adicionar Contato</v-card-title>
                 <v-card-text>
                     <v-form @submit.prevent="createContato">
-                        <div class="grid gap-4 md:grid-cols-2">
+                        <div class="grid gap-4 sm:grid-cols-1 md:grid-cols-2">
                             <v-text-field
                                 v-model="form.nome"
                                 label="Nome"
@@ -99,11 +99,22 @@ const formatErrors = (errorResponse) => {
                                 @change="previewImage"
                                 :error-messages="errors.imagem || []"
                             ></v-file-input>
-                            <div v-if="imagePreview" class="col-span-2">
-                                <img :src="imagePreview" alt="Pré-visualização" class="max-w-full h-auto" />
+                            <div
+                                v-if="imagePreview"
+                                class="md:col-span-2 flex justify-center align-center"
+                            >
+                                <div class="w-1/2">
+                                    <img
+                                        :src="imagePreview"
+                                        alt="Pré-visualização"
+                                        class="max-w-full h-auto"
+                                    />
+                                </div>
                             </div>
                         </div>
-                        <v-btn type="submit" color="primary" class="mt-4">Adicionar</v-btn>
+                        <v-btn type="submit" color="primary" class="mt-4"
+                            >Adicionar</v-btn
+                        >
                     </v-form>
                 </v-card-text>
             </v-card>
