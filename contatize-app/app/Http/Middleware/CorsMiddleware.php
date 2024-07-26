973ce65a347019a5d9c07753c6915e7f2fcf3c83
Hugobsan <hugocorone10@gmail.com>
2024-07-26 16:34:22 -0300

    fix: Implementando o CORS

--- contatize-app/app/Http/Middleware/CorsMiddleware.php
+++ contatize-app/app/Http/Middleware/CorsMiddleware.php
@@ -11,15 +11,26 @@ class CorsMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        header("Access-Control-Allow-Origin: *");
+
+        $headers = [
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, PATCH, DELETE',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization'
+        ];
+        if ($request->getMethod() == "OPTIONS") {
+            return response('OK')
+                ->withHeaders($headers);
+        }
+
+        $response = $next($request);
+        foreach ($headers as $key => $value)
+            $response->header($key, $value);
+        return $response;
     }
 }
--- contatize-app/routes/web.php
+++ contatize-app/routes/web.php
@@ -8,21 +8,26 @@ use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Middleware\CorsMiddleware;
 
-Route::get('/', function () {
-    return redirect()->route('login');
-});
 
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/login', [AuthController::class, 'authenticate'])->name('autenticar');
+//Adicionando Group para o middleware de CorsMiddleware
+Route::group(function () {
+    Route::get('/', function () {
+        return redirect()->route('login');
+    });
 
-Route::resource('users', UserController::class)->only(['create', 'store']);
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'authenticate'])->name('autenticar');
 
-Route::group(['middleware' => 'auth'], function () {
-    Route::resource('contatos', ContatoController::class);
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::put('password', [PasswordController::class, 'update'])->name('password.alterar');
-});
+    Route::resource('users', UserController::class)->only(['create', 'store']);
+
+    Route::group(['middleware' => 'auth'], function () {
+        Route::resource('contatos', ContatoController::class);
+        Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        Route::put('password', [PasswordController::class, 'update'])->name('password.alterar');
+    });
+})->middleware(CorsMiddleware::class);
